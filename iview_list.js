77f2f2916fe353cd34be3095b8e4370d2e7c65da
Robert Holland <rob@robholland.com>
2014-10-25 10:32:05 -0700

    Added iview_list1() function.

--- iview_list.js
+++ iview_list.js
@@ -31,3 +31,44 @@ function iviewlist(){
 	xmlhttp.close();
 
 }
+
+function iviewlist1(){
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+		xmlhttp=new XMLHttpRequest();
+		}
+	else {
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+	xmlhttp.onreadystatechange=function(){
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{document.getElementById("iviewdisplay").innerHTML=xmlhttp.responseText;
+		}
+		}
+//		alert("Made it here!");
+		var x = document.getElementById("optiondown").selectedIndex;
+		y=(document.getElementsByTagName("option")[x].value);
+//		alert(y);
+		if (y==null || y=="")
+  		{
+  		alert("Program selection must not be blank!");
+  		return false;
+  		}
+
+	var programtosend=document.getElementsByTagName("option")[x].value;
+	//alert(programtosend);
+	xmlhttp.open("POST","iview_list1.php",true);
+	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.send("optiondown="+programtosend+"");
+//	xmlhttp.send();
+	xmlhttp.close();
+
+}
+
+
+$(document).ready(function()
+    {
+        $("#myTable").tablesorter();
+    }
+);
