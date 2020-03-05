1e966ce364aed3d4d108b95ae0bbfdff03b911a5
Robert Holland <Robert.Holland@bannerhealth.com>
2020-03-05 15:16:01 -0700

    Adding military timestamp script.

---
+++ scripts/timestamp_mil.js
@@ -0,0 +1,40 @@
+//Get current datetime.
+var now = new Date();
+
+//Assign time parts.
+var month = (now.getMonth());
+month = month + 1; //Adding one to Month. Month is zero based. Example: September shows as 8 instead of 9.
+  if (month < 10) {
+     month = "0" + month
+  }
+
+var day = (now.getDate());
+  if (day < 10) {
+     day = "0" + day
+  }
+
+var hours = (now.getHours());
+  if (hours < 10) {
+     hours = "0" + hours
+  }
+
+var minutes = (now.getMinutes());
+  if (minutes < 10) {
+     minutes = "0" + minutes
+  }
+
+var seconds = (now.getSeconds());
+  if (seconds < 10) {
+     seconds = "0" + seconds
+  }
+
+//Write the full military time to the document. Format is: YearMonthDayHourMinuteSecond.
+UltraEdit.activeDocument.write(
+				now.getFullYear()
+      + month
+      + day
+//      + "-"
+      + hours
+      + minutes
+      + seconds
+      + " ");
