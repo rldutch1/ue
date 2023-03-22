65cd8613880d5b2f4fdb48cd4ade31b00bd2387e
Robert Holland <Robert.Holland@bannerhealth.com>
2023-03-22 09:59:58 -0700

    Adding modified files/templates.

--- backup.sh
+++ backup.sh
@@ -4,6 +4,6 @@ for file in UETMPLTE.TPL.*
 		cat UETMPLTE.TPL > $file
 		echo $file " updated."
 	done
-	tar -zcvf ./templates/UETMPLTE_Backup.tar.gz UETMPLTE.TPL UETMPLTE.TPL.txt UETMPLTE.TPL.bak
+	tar -zcvf UETMPLTE_Backup.tar.gz UETMPLTE.TPL UETMPLTE.TPL.txt UETMPLTE.TPL.bak
 cd ../
 git commit -am "Adding modified files/templates."
