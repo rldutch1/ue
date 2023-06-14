6ac328d60d54f9e614bcd6a5ec441767c314f50e
Robert Holland <Robert.Holland@bannerhealth.com>
2023-06-13 17:13:37 -0700

    Update to csvToJSON script and shutdown template.

--- scripts/csvToJSON.js
+++ scripts/csvToJSON.js
@@ -1,10 +1,21 @@
+// ----------------------------------------------------------------------------
+// Script Author: Robert Holland
+// Script Name: csvToJSON.js
+// Creation Date: Sat Jun 03 2023 10:26:02 GMT-0700 (US Mountain Standard Time)
+// Source: https://www.youtube.com/watch?v=AOV-MSW4CsE
+// Copyright (c)2023
+// Version: 1.0.0
+// Purpose: UE script to convert CSV data to JSON data. Modified to put JSON data in a new window.
+// ----------------------------------------------------------------------------
 
+// @Engine=WebView2
 var csv_data = UltraEdit.activeDocument.selection;
 var converted_data = csvToJSON(csv_data);
 
 UltraEdit.outputWindow.showWindow(true);
 UltraEdit.outputWindow.write(converted_data);
-//UltraEdit.activeDocument.write(converted_data);
+UltraEdit.newFile();
+UltraEdit.activeDocument.write(converted_data);
 
 function csvToJSON(csvData){
 	const lines = csvData.trim().split('\n');
